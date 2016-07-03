7bc63050aa5f05f124d4eefc4f6c883ef0b58aaf
Rick Toews <rick.toews@gmail.com>
2016-07-02 23:53:53 -0700

    Try removing class to external file.

---
+++ classes/math_playground.rb
@@ -0,0 +1,22 @@
+class MathPlayground
+  def tri(n)
+  	n*(n+1)/2
+  end
+
+  def tri_lte(max)
+  	max = max.to_i
+  	tris = []
+  	i = 1
+  	begin
+  		tmp = tri(i)
+  		if (tmp <= max)
+  			tris.push(tmp)
+  		end
+  		i = i + 1
+  	end while tmp <= max
+  	out = {
+      "tri" => tris
+    }
+    out.to_json
+  end
+end
--- rest.rb
+++ rest.rb
@@ -1,28 +1,6 @@
 require 'sinatra'
 require 'json'
-
-class MathPlayground
-  def tri(n)
-  	n*(n+1)/2
-  end
-
-  def tri_lte(max)
-  	max = max.to_i
-  	tris = []
-  	i = 1
-  	begin
-  		tmp = tri(i)
-  		if (tmp <= max)
-  			tris.push(tmp)
-  		end
-  		i = i + 1
-  	end while tmp <= max
-  	out = {
-      "tri" => tris
-    }
-    out.to_json
-  end
-end
+require 'classes/math_playground'
 
 math = MathPlayground.new
 get '/tri/:n' do
