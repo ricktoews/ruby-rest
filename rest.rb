afcf8bdf7fe8600e5e7786ad417d4d75a75f04b8
Rick Toews <rick.toews@gmail.com>
2016-07-02 23:42:23 -0700

    Put methods in a class.

--- rest.rb
+++ rest.rb
@@ -1,27 +1,30 @@
 require 'sinatra'
 require 'json'
 
+math = new Math
 get '/tri/:n' do
-  tri_lte(params[:n])
+  math.tri_lte(params[:n])
 end
 
-def tri(n)
-	n*(n+1)/2
-end
+class Math
+  def tri(n)
+  	n*(n+1)/2
+  end
 
-def tri_lte(max)
-	max = max.to_i
-	tris = []
-	i = 1
-	begin
-		tmp = tri(i)
-		if (tmp <= max)
-			tris.push(tmp)
-		end
-		i = i + 1
-	end while tmp <= max
-	out = {
-    "tri" => tris
-  }
-  out.to_json
+  def tri_lte(max)
+  	max = max.to_i
+  	tris = []
+  	i = 1
+  	begin
+  		tmp = tri(i)
+  		if (tmp <= max)
+  			tris.push(tmp)
+  		end
+  		i = i + 1
+  	end while tmp <= max
+  	out = {
+      "tri" => tris
+    }
+    out.to_json
+  end
 end
