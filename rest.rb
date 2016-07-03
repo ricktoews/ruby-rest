79fa687b3298eff3e2dd343154d2e94d7b829861
Rick Toews <rick.toews@gmail.com>
2016-07-03 00:08:42 -0700

    troubleshooting

--- rest.rb
+++ rest.rb
@@ -6,5 +6,6 @@ require './classes/payload'
 math = MathPlayground.new
 payload = Payload.new
 get '/tri/:n' do
-  payload.output_json(math.tri_lte(params[:n]))
+  math.tri_lte(params[:n])
+#  payload.output_json(math.tri_lte(params[:n]))
 end
